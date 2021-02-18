8b0aec3fea22a053f3447e0e36199dafd09effa7
1392782 <79203565+1392782@users.noreply.github.com>
2021-02-18 21:55:31 +0900

    Create 1392782.sol

---
+++ 1392782.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.2;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.1.0/contracts/token/ERC20/ERC20.sol";
+
+contract X1392782 is ERC20 {
+    constructor () public ERC20("X1392782", "X1392782") {
+        _mint(msg.sender, 10 ** uint256(decimals()));
+    }
+}
